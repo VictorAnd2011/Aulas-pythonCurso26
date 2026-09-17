96c501c204fea04bd8f8c250fbbc581311ed8339
VictorAnd2011 <victorandrade20110906@gmail.com>
2026-09-16 22:11:04 -0300

    Update loopWhile.py

--- Aulas_python/basico do python/loopWhile.py
+++ Aulas_python/basico do python/loopWhile.py
@@ -1,6 +1,8 @@
 d = "s"
 while d.lower().startswith("s"):
+    
     d = input("Deseja continuar? [s]im: ")
-    print("Você continuou!")
+    if d.lower().startswith("s"):
+        print("Você continuou!")
 
 print("Você saiu! ")
