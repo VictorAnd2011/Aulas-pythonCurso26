c2dfb50bb38d46102f246828debbbe26f94813b8
VictorAnd2011 <victorandrade20110906@gmail.com>
2026-09-19 13:16:37 -0300

    Update Listas.py

--- Aulas_python/basico do python/tipos_de_variaveis/Listas.py
+++ Aulas_python/basico do python/tipos_de_variaveis/Listas.py
@@ -67,3 +67,57 @@ print(lista2)  # []
 #o insert() - insere um elemento em um índice específico
 lista.insert(0, 'Acerola') #primeiro parâmetro é o índice, segundo é o elemento
 print(lista)  # ['Acerola', 'Banana', 'Danone', 'Espinafre', 'Figo']
+
+
+#é possivel concatenar listas com o operador +
+lista.clear()
+lista1 = [1, 2, 3]
+lista2 = [4, 5, 6]
+lista3 = lista1 + lista2
+print(lista3)  # [1, 2, 3, 4, 5, 6]
+
+#é possivel concatenar também com o extend(), continuando a lista original
+lista1.extend(lista3)
+print(lista1)  # [1, 2, 3, 1, 2, 3, 4, 5, 6]
+
+#listas são iteráveis, então posso usar o for para percorrer cada elemento da lista
+for item in lista3:
+    print(item)
+
+
+#o len retorna a quantidade de elementos da lista, dá pra usar isso para ver o indice e o elemento ao mesmo tempo
+indices = range(len(lista1))
+for indice in indices:
+    print(f'Índice: {indice}, Elemento: {lista1[indice]}')
+
+
+'''
+resumindo tudo: 
+- Listas são mutáveis
+- Suportam vários tipos de dados
+- Podem ser concatenadas com + ou extend()
+- Elementos podem ser acessados por índice
+- Elementos podem ser removidos com del, pop() ou clear()
+- Elementos podem ser adicionados com append() ou insert()
+'''
+
+lista1.clear()
+lista2.clear()
+lista3.clear()
+
+#CUIDADOS PARA TOMAR COM LISTAS
+#1 - Listas são mutáveis, então se você colocar uma lista apontando para outra,]
+# qualquer alteração na primeira vai alterar a segunda também, pois elas estão apontando para o mesmo lugar na memória
+lista = ['Maria', 'João', 'Carlos']
+lista2 = lista  # lista2 aponta para o mesmo lugar na memória que lista
+print(lista2)  # ['Maria', 'João', 'Carlos']]
+
+#Agora, se eu alterar um elemento da lista, a lista2 também vai ser alterada
+lista[0] = 'Ana'
+print(lista2)  # ['Ana', 'João', 'Carlos']
+
+#Para evitar isso, posso usar o método copy() para criar uma cópia da lista
+lista3 = lista.copy()
+lista[0] = 'Qualquer coisa'
+print(lista)  # ['Qualquer coisa', 'João', 'Carlos']
+print(lista3)  # ['Ana', 'João', 'Carlos'] - a lista3 não foi alterada, pois ela é apenas uma cópia da lista original
